5fc3dbd65768623b513df950f927c11d90548c6b
reddawn-ux <69806077+reddawn-ux@users.noreply.github.com>
2023-09-06 12:43:11 +0300

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,14 @@
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$ErrorActionPreference = 'SilentlyContinue'
+$wshell = New-Object -ComObject Wscript.Shell
+$Button = [System.Windows.MessageBoxButton]::YesNoCancel
+$ErrorIco = [System.Windows.MessageBoxImage]::Error
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	Exit
+}
+Write-Host "Checking winget..."
+
+Start-Process "https://google.com/"
